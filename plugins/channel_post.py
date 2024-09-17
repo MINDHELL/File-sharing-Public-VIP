5b719392ba5e5c190231bb6052d0a62a561667d5
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-09-17 11:35:13 -0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -13,7 +13,7 @@ from helper_func import encode
 
 
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','upi','stats','removepr','addpr']))
+@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','upi', 'myplan' , 'plans' ,'stats','removepr','addpr']))
 async def channel_post(client: Client, message: Message):
     reply_text = await message.reply_text("Please Wait...!", quote = True)
     try:
