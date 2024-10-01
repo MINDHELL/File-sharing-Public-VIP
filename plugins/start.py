983f13a0ad5af473a8d84415804d8a14cc1a2c04
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-10-01 05:22:11 -0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -375,23 +375,24 @@ async def check_tokens_callback(client: Client, callback_query: CallbackQuery):
     )
 
 
-# Existing /start Not Joined Handler
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
     buttons = [
         [
-            InlineKeyboardButton(
-                "Join Channel",
-                url=client.invitelink
-            )
+            InlineKeyboardButton(text="Join Channel", url=client.invitelink),
+            #InlineKeyboardButton(text="Join Channel", url=client.invitelink2),
+        ],
+        [
+            InlineKeyboardButton(text="Join Channel", url=client.invitelink3),
+            #InlineKeyboardButton(text="Join Channel", url=client.invitelink4),
         ]
     ]
     try:
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text='Try Again',
-                    url=f"https://t.me/{client.username}?start={message.command[1]}"
+                    text = 'Try Again',
+                    url = f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]
         )
@@ -399,16 +400,16 @@ async def not_joined(client: Client, message: Message):
         pass
 
     await message.reply(
-        text=FORCE_MSG.format(
-            first=message.from_user.first_name,
-            last=message.from_user.last_name,
-            username=None if not message.from_user.username else '@' + message.from_user.username,
-            mention=message.from_user.mention,
-            id=message.from_user.id
-        ),
-        reply_markup=InlineKeyboardMarkup(buttons),
-        quote=True,
-        disable_web_page_preview=True
+        text = FORCE_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ),
+        reply_markup = InlineKeyboardMarkup(buttons),
+        quote = True,
+        disable_web_page_preview = True
     )
 
 WAIT_MSG = """"<b>Processing ...</b>"""
