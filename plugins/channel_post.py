b71e7092803f3f0187c7d56c8905982b7d74a746
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-10-19 14:05:46 -0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -14,29 +14,37 @@ from helper_func import encode
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','upi', 'myplan' , 'plans' ,'stats','removepr','addpr']))
-#@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
 async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("Please Wait...!", quote = True)
+    reply_text = await message.reply_text("Please Wait...!", quote=True)
+    
     try:
-        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
+        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
     except FloodWait as e:
         await asyncio.sleep(e.x)
-        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
+        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
     except Exception as e:
         print(e)
-        await reply_text.edit_text("Something went Wrong..!")
+        await reply_text.edit_text("Something went wrong!")
         return
+
     converted_id = post_message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
+    
+    # Generate normal and premium links
+    normal_link = f"https://t.me/{client.username}?start={base64_string}"
+    premium_link = f"https://t.me/{client.username}?start=premium_{base64_string}"
+    
+    reply_markup = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("🔁 Share Normal URL", url=f'https://telegram.me/share/url?url={normal_link}'),
+          InlineKeyboardButton("🔁 Share Premium URL", url=f'https://telegram.me/share/url?url={premium_link}')]]
+    )
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-
-    await reply_text.edit(f"<b>Here is your link</b>\n\n{link} \n\n join @ultroid_official", reply_markup=reply_markup, disable_web_page_preview = True)
+    await reply_text.edit(f"<b>Here are your links:</b>\n\nNormal: {normal_link} \nPremium: {premium_link} \n\nJoin @ultroid_official", reply_markup=reply_markup, disable_web_page_preview=True)
 
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
+
 
 
 
