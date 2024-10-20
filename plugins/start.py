db58ac8f5b6925bd92c0b21cf6465b61973d98d4
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-10-20 01:30:19 -0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -93,7 +93,7 @@ async def auto_delete_message(client, chat_id, message_id, delay=3600):  # Set d
 
 
 
-@Bot.on_message(filters.command('start') & filters.private)
+@Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
 
